a62245efbb0e43de21908cfb3a1ef3bbb2699637
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2026-08-17 15:40:10 +1000

    chore: rebuild ui

--- spinifex/services/spinifexui/frontend/dist/assets/commandBuilder.js
+++ spinifex/services/spinifexui/frontend/dist/assets/commandBuilder.js
@@ -1,1 +1,1 @@
-import{At as e,dt as t}from"./cluster-config.js";import{Mn as n}from"./awsClient.js";var r=e(n,`AWSWesleyFrontend`,`EKSClient`,t),i={},a=(e,t,n,r)=>[];export{a as n,r,i as t};
+import{At as e,dt as t}from"./cluster-config.js";import{Yn as n}from"./awsClient.js";var r=e(n,`AWSWesleyFrontend`,`EKSClient`,t),i={},a=(e,t,n,r)=>[];export{a as n,r,i as t};

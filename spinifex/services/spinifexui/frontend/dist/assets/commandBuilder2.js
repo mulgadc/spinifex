e90dfa4ea36379504e6a1cc8f9cac697a9a637e0
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2026-08-31 09:24:56 +1000

    chore: rebuild ui with latest deps

--- spinifex/services/spinifexui/frontend/dist/assets/commandBuilder2.js
+++ spinifex/services/spinifexui/frontend/dist/assets/commandBuilder2.js
@@ -1,1 +1,1 @@
-import{Lt as e,_t as t}from"./cluster-config.js";import{_a as n}from"./awsClient.js";var r=e(n,`AmazonBedrockFrontendService`,`BedrockRuntimeClient`,t),i={},a=(e,t,n,r)=>[];export{a as n,r,i as t};
+import{Ft as e,gt as t}from"./cluster-config.js";import{_a as n}from"./awsClient.js";var r=e(n,`AmazonBedrockFrontendService`,`BedrockRuntimeClient`,t),i={},a=(e,t,n,r)=>[];export{a as n,r,i as t};
--- spinifex/services/spinifexui/frontend/dist/assets/commandBuilder4.js
+++ spinifex/services/spinifexui/frontend/dist/assets/commandBuilder4.js
@@ -1,1 +1,1 @@
-import{Lt as e,_t as t}from"./cluster-config.js";import{Rn as n}from"./awsClient.js";var r=e(n,`ElasticLoadBalancing_v10`,`ElasticLoadBalancingV2Client`,t),i={},a=(e,t,n,r)=>[];export{a as n,r,i as t};
+import{Ft as e,gt as t}from"./cluster-config.js";import{Rn as n}from"./awsClient.js";var r=e(n,`ElasticLoadBalancing_v10`,`ElasticLoadBalancingV2Client`,t),i={},a=(e,t,n,r)=>[];export{a as n,r,i as t};
